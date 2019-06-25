4ca8bac5a50330d52d3248fd7b2961248f68b4ab
fengsonga <973017388@qq.com>
2019-06-25 11:39:34 +0800

    Update home.js

--- miniprogram/page/myPages/home/home.js
+++ miniprogram/page/myPages/home/home.js
@@ -8,7 +8,22 @@ Page({
       path: 'page/component/index'
     }
   },
-
+  onLoad: function() {
+    // 可以通过 wx.getSetting 先查询一下用户是否授权了 "scope.record" 这个 scope
+    wx.getSetting({
+      success(res) {
+        if (!res.authSetting['scope.userInfo']) {
+          wx.authorize({
+            scope: 'scope.userInfo',
+            success () {
+              // 用户已经同意小程序使用录音功能，后续调用 wx.startRecord 接口不会弹窗询问
+              // wx.startRecord()
+            }
+          })
+        }
+      }
+    })
+  },
   data: {
     list: [
       {
