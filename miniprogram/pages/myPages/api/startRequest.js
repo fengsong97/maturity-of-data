a3575310001be85b141ffb63b03b120f4a000184
冯松 <fengsong97@qq.com>
2019-10-29 14:07:06 +0800

    Revert "Update startRequest.js"
    This reverts commit 93330be13cc680d04fd876c14288822dba0e04c9.

--- miniprogram/pages/myPages/api/startRequest.js
+++ miniprogram/pages/myPages/api/startRequest.js
@@ -1,17 +1,17 @@
  var app = getApp();
- //��ĿURL��ͬ���֣������������ͬʱ������ĿǨ��
- //������Ϊ���Ǳ��ص��ԣ�����host���淶��ʵ����Ӧ�����㱸����������Ϣ
+ //项目URL相同部分，减轻代码量，同时方便项目迁移
+ //这里因为我是本地调试，所以host不规范，实际上应该是你备案的域名信息
  var host = 'https://a.fuduo.wang/'; 
  /**
-  * POST����
-  * URL���ӿ�
-  * postData��������json����
-  * doSuccess���ɹ��Ļص�����
-  * doFail��ʧ�ܵĻص�����
+  * POST请求，
+  * URL：接口
+  * postData：参数，json类型
+  * doSuccess：成功的回调函数
+  * doFail：失败的回调函数
   */
  function postData(url, postData, doSuccess, doFail) {
    wx.request({
-     //��Ŀ�������ӿڣ�ͨ���ַ���ƴ�ӷ�ʽʵ��
+     //项目的真正接口，通过字符串拼接方式实现
      url: host + url,
      header: {
        "content-type": "application/json;charset=UTF-8"
@@ -19,7 +19,7 @@
      data: postData,
      method: 'POST',
      success: function (res) {
-       //����ֵΪres.data,ֱ�ӽ����ص����ݴ���
+       //参数值为res.data,直接将返回的数据传入
        doSuccess(res.data);
      },
      fail: function () {
@@ -28,7 +28,7 @@
    })
  }
  
- //GET���󣬲��贫�Σ�ֱ��URL���ã�
+ //GET请求，不需传参，直接URL调用，
  function getData(url, doSuccess, doFail) {
    wx.request({
      url: host + url,
@@ -46,10 +46,10 @@
  }
  
  /**
-  * module.exports������������
-  * js�ļ���ͨ��var call = require("../util/request.js")  ����
-  * �����������ļ���ʱ��"  "���������ͨ��../../../�������ͣ�С����ı��������Զ���ʾ����Ϊ�����
-  * ��ĿĿ¼��ֹһ������ͬ��js�ļ���Ӧ�Ĺ������λ�ò�һ��
+  * module.exports用来导出代码
+  * js文件中通过var call = require("../util/request.js")  加载
+  * 在引入引入文件的时候"  "里面的内容通过../../../这种类型，小程序的编译器会自动提示，因为你可能
+  * 项目目录不止一级，不同的js文件对应的工具类的位置不一样
   */
  module.exports.postData = postData;
  module.exports.getData = getData;
